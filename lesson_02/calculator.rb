580b92995c484a461ffae81c9e9db9ea2903c9a2
Dan Beuthling <danbeuthling@gmail.com>
2016-02-15 00:58:58 +0000

    refactured calculator after rubocop

--- lesson_02/calculator.rb
+++ lesson_02/calculator.rb
@@ -1,16 +1,16 @@
-#ask the user for two numbers
-#ask the user for an operation to perform
-#perfom the operation on the two numbers
-#output the result
+# ask the user for two numbers
+# ask the user for an operation to perform
+# perfom the operation on the two numbers
+# output the result
 
-#answer = Kernel.gets()
-#Kernel.puts(answer)
+# answer = Kernel.gets()
+# Kernel.puts(answer)
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
-def valid_nubmer?(num)
+def valid_number?(num)
   num.to_i() != 0
 end
 
@@ -33,42 +33,30 @@ name = ''
 loop do
   name = Kernel.gets().chomp()
 
-  if name.empty?()
-    prompt("I didn't catch your name.")
-  else
-    break
-  end
+  break unless name.empty?()
+  prompt("I didn't catch your name.")
 end
 
 prompt("Hello #{name}, let's calculate.")
 
-loop do #main loop
-
-
-  number1 = nil
+loop do # main loop
+  number1 = ''
   number2 = ''
 
   loop do
     prompt("What is the first number?")
     number1 = Kernel.gets().chomp()
 
-    if valid_nubmer?(number1)
-      break
-    else
-      prompt("That doesn't look like a valid number.")
-    end
+    break unless valid_number?(number1) == false
+    prompt("That doesn't look like a valid number!")
   end
-
 
   loop do
     prompt("What is the second number?")
     number2 = Kernel.gets().chomp()
 
-    if valid_nubmer?(number2)
-      break
-    else
-      prompt("That doesn't look like a valid number.")
-    end
+    break unless valid_number?(number2) == false
+    prompt("That doesn't look like a valid number.")
   end
   operator_prompt = <<-message
     What operation would you like to perform?
@@ -84,29 +72,22 @@ loop do #main loop
   loop do
     operator = Kernel.gets().chomp()
 
-    if %w(1 2 3 4).include?(operator)
-      break
-    else
-      prompt("Must choose 1 2 3 or 4")
-    end
+    break unless %w(1 2 3 4).include?(operator) == false
+    prompt("Must choose 1 2 3 or 4")
   end
 
   prompt("#{operation_to_message(operator)} the two numbers...")
 
   result = case operator
-  when '1'
-    #prompt("Adding!")
-    number1.to_i() + number2.to_i()
-  when '2'
-    #prompt("Subtracting!")
-    number1.to_i() - number2.to_i()
-  when '3'
-    #prompt("Mulitplying!")
-    number1.to_i() * number2.to_i()
-  when '4'
-    #prompt("Dividing!")
-    number1.to_i() / number2.to_f()
-  end
+           when '1'
+             number1.to_i() + number2.to_i()
+           when '2'
+             number1.to_i() - number2.to_i()
+           when '3'
+             number1.to_i() * number2.to_i()
+           when '4'
+             number1.to_i() / number2.to_f()
+           end
 
   prompt("The result is #{result}.")
 

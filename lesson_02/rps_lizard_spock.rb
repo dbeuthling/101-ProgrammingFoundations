77a8e315b32fd1fa640f6a4c06f8d09f5a45bdfc
Dan Beuthling <danbeuthling@gmail.com>
2016-02-19 23:15:58 -0600

    finished rock paper scissors lizard spock

--- lesson_02/rps_lizard_spock.rb
+++ lesson_02/rps_lizard_spock.rb
@@ -1,8 +1,14 @@
 VALID_CHOICES = %w(rock paper scissors lizard spock).freeze
 
-# def test_method(something)
-#   prompt("#{something}")
-# end
+def expand_abbr(letter)
+  case letter
+  when 'r' then 'rock'
+  when 'p' then 'paper'
+  when 's' then 'scissors'
+  when 'l' then 'lizard'
+  when 'sp' then 'spock'
+  end
+end
 
 def win?(first, second)
   (first == 'rock' && (second == 'scissors' || second == 'lizard')) ||
@@ -12,39 +18,14 @@ def win?(first, second)
     (first == 'spock' && (second == 'rock' || second == 'scissors'))
 end
 
-def expand_abbr(letter)
-  case letter
-  when 'r'
-    'rock'
-  when 'p'
-    'paper'
-  when 'sc'
-    'scissors'
-  when 'l'
-    'lizard'
-  when 'sp'
-    'spock'
-  end
-end
-
-$player_score = 0
-$computer_score = 0
-
-
 def display_results(player, computer)
   if win?(player, computer)
-    $player_score += 1
-    # if $computer_score > 0 then $computer_score -=1 end
     prompt("YOU WIN! You chose #{player}, and the computer chose #{computer}.")
   elsif win?(computer, player)
-    $computer_score += 1
-    # if $player_score > 0 then $player_score -=1 end
     prompt("YOU LOST! You chose #{player}, and the computer chose #{computer}.")
   else prompt("TIE! You chose #{player}, and the computer chose #{computer}.")
   end
-  #prompt("Score is: Computer - #{computer_score}  You - #{player_score}")
 end
-# p test_method('boofar')
 
 def prompt(message)
   puts "=> #{message}"
@@ -52,7 +33,10 @@ end
 
 choice = ''
 computer_choice = ''
+player_score = 0
+computer_score = 0
 
+prompt("First to 5 wins is Champion of the World!")
 loop do
   loop do
     prompt("Please choose one (enter the letters inside the parenthesis): (r)ock, (p)aper, (s)cissors, (l)izard, (sp)ock")
@@ -64,10 +48,27 @@ loop do
 
   computer_choice = VALID_CHOICES.sample
   display_results(choice, computer_choice)
-p "You #{$player_score}, computer: #{$computer_score}"
 
-  prompt("Would you like to play again?")
-  again = gets.chomp
-  break unless again.downcase.start_with?('y')
+  if win?(choice, computer_choice)
+    player_score += 1
+  elsif win?(computer_choice, choice)
+    computer_score += 1
+  end
+
+  p "You have: #{player_score}, computer has: #{computer_score}"
+
+  if player_score > 4 || computer_score > 4
+    if player_score < computer_score
+      prompt("The computer is the champ!")
+    else
+      prompt("You are the champ!")
+    end
+    player_score = 0
+    computer_score = 0
+    prompt("Would you like to play again?")
+    again = gets.chomp
+    break unless again.downcase.start_with?('y')
+  end
 end
+
 prompt("Thanks for playing! Good-bye!")

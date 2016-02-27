1b8fe0e9612de7f3f03ae8f972be1c8ccadeaf66
Dan Beuthling <danbeuthling@gmail.com>
2016-02-27 15:23:00 -0600

    Completed exercises medium 2

--- lesson_03/exercises_medium2.rb
+++ lesson_03/exercises_medium2.rb
@@ -53,3 +53,64 @@ my_string, my_array = not_so_tricky_method(my_string, my_array)
 
 puts "My string looks like this now: #{my_string}"
 puts "My array looks like this now: #{my_array}"
+
+# Question 4
+sentence = "Humpty Dumpty sat on a wall"
+p sentence.split.reverse!.join(" ") + "."
+
+# Question 5
+answer = 42
+
+def mess_with_it(some_number)
+  some_number += 8
+end
+
+new_answer = mess_with_it(answer)
+
+p answer - 8 # should be 34 since method not invoked
+p new_answer # should be 50 since method is invoked -
+
+# Question 6
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => {"age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female" }
+}
+
+def mess_with_demographoics(demo_hash)
+  demo_hash.values.each do |family_member|
+    family_member["age"] += 42
+    family_member["gender"] = "other"
+  end
+end
+
+mess_with_demographoics(munsters)
+p munsters # permanently alters the hash because demo_hash points to munsters
+
+# Question 7
+def rps(fist1,fist2)
+  if fist1 == "rock"
+    (fist2 == "paper") ? "paper" : "rock"
+  elsif fist1 == "paper"
+    (fist2 == "scissors") ? "scissors" : "paper"
+  else
+    (fist2 == "rock") ? "rock" : "scissors"
+  end
+end
+
+puts rps(rps(rps("rock", "paper"), rps("rock", "scissors")), "rock")
+# result is paper following order of operations
+
+# Question 8
+def foo(param = "no")
+  "yes"
+end
+
+def bar(param = "no")
+  param == "no" ? "yes" : "no"
+end
+
+p bar(foo) # is "no" because foo will always be "yes"
+

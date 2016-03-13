a440cf344cfd868d00f51024dd54ce11852b611b
Dan Beuthling <danbeuthling@gmail.com>
2016-03-12 22:00:10 -0600

    Further 21 - Issue with Aces

--- lesson_04/21.rb
+++ lesson_04/21.rb
@@ -1,44 +1,95 @@
-full_deck = [['H', '2'], ['H', '3'],['H', '4'], ['H', '5'], ['H', '6'], ['H', '7'], ['H', '8'], ['H', '9'], ['H', '10'], ['H', 'J'],['H', 'Q'], ['H', 'K'], ['H', 'A']] +
-			[['S', '2'], ['S', '3'],['S', '4'], ['S', '5'], ['S', '6'], ['S', '7'], ['S', '8'], ['S', '9'], ['S', '10'], ['S', 'J'],['S', 'Q'], ['S', 'K'], ['S', 'A']] +
-			[['C', '2'], ['C', '3'],['C', '4'], ['C', '5'], ['C', '6'], ['C', '7'], ['C', '8'], ['C', '9'], ['C', '10'], ['C', 'J'],['C', 'Q'], ['C', 'K'], ['C', 'A']] +
-			[['D', '2'], ['D', '3'],['D', '4'], ['D', '5'], ['D', '6'], ['D', '7'], ['D', '8'], ['D', '9'], ['D', '10'], ['D', 'J'],['D', 'Q'], ['D', 'K'], ['D', 'A']]
+require 'pry'
 
-values = {"A"=>[1, 11], "2"=>2, "3"=>3, "4"=>4, "5"=>5, "6"=>6, "7"=>7, "8"=>8, "9"=>9, "10"=>10, "J"=>10, "Q"=>10, "K"=>10}
+full_deck = [['H', '2'], ['H', '3'],['H', '4'], ['H', '5'], ['H', '6'], ['H', '7'], ['H', '8'], ['H', '9'], ['H', '10'], ['H', 'Jack'],['H', 'Queen'], ['H', 'King'], ['H', 'Ace']] +
+			[['S', '2'], ['S', '3'],['S', '4'], ['S', '5'], ['S', '6'], ['S', '7'], ['S', '8'], ['S', '9'], ['S', '10'], ['S', 'Jack'],['S', 'Queen'], ['S', 'King'], ['S', 'Ace']] +
+			[['C', '2'], ['C', '3'],['C', '4'], ['C', '5'], ['C', '6'], ['C', '7'], ['C', '8'], ['C', '9'], ['C', '10'], ['C', 'Jack'],['C', 'Queen'], ['C', 'King'], ['C', 'Ace']] +
+			[['D', '2'], ['D', '3'],['D', '4'], ['D', '5'], ['D', '6'], ['D', '7'], ['D', '8'], ['D', '9'], ['D', '10'], ['D', 'Jack'],['D', 'Queen'], ['D', 'King'], ['D', 'Ace']]
 
+VALUES = {"Ace"=>11, "2"=>2, "3"=>3, "4"=>4, "5"=>5, "6"=>6, "7"=>7, "8"=>8, "9"=>9, "10"=>10, "Jack"=>10, "Queen"=>10, "King"=>10}
+
+deck = full_deck
+
+def prompt(msg)
+	puts "=> #{msg}"
+end
 
 def players_cards(deck)
 	hand = deck.sample(2)
 	rank = hand.collect {|card| card[1] }
-	rank[0] + " and " + rank[1]
+	cards = [rank[0] , rank[1]]
+
 end
+
+def hit(cards)
+	card = cards.sample.values_at(1)
+	prompt("You drew a #{card.first}")
+	find_value(card)
+end
+
 
 def dealer_cards(deck)
 	hand = deck.sample(2)
 	rank = hand.collect {|card| card[1] }
-	rank[0] + " and a hidden card"
+	cards = [rank[0] , rank[1]]
 end
 
-puts "You have #{players_cards(full_deck)}."
-puts "Dealer has #{dealer_cards(full_deck)}."
+def dealer_starting_hand(deck)
+	dealer_holding = dealer_cards(deck)
+	prompt("Dealer has: #{dealer_holding[0]} and a hidden card.")
+	find_value(dealer_holding)
+end
 
 
 
+def player_starting_hand(deck)
+	player_holding = players_cards(deck)
+	prompt("You have: #{player_holding[0]} and #{player_holding[1]}")
+	prompt("For a total of #{find_value(player_holding)}")
+	find_value(player_holding)
+end
 
-# cards = %w(2 3 4 5 6 7 8 9 10 J Q K A)
-# suits = %w(S D C H)
+def find_value(cards)
+	value=0
+	cards.each do |card|
+		value += VALUES.fetch(card)
+		if card == 'Ace' && value > 21
+			value -= 10
+		end
+	end
+	value
+end
 
-# p cards.combination(2).to_a.sample(1)
+def busted?(hand)
+	if hand > 21
+		return true
+	else
+		return false
+	end
+end
 
-# def combine(suit, card)
-# 	deck = []
-# 	suit.each do |suit|
-# 		card.each do |card|
-# 			deck << [suit] 
-# 			deck << [card]
-# 		end
-# 	end
-# 	suit
+dealer_hand = dealer_starting_hand(deck)
+player_hand = player_starting_hand(deck)
+dealer_hand
 
-# 	end
-# p combine(suits, cards)
-# p full_deck
+
+
+answer = nil
+loop do
+	break if busted?(player_hand)
+  prompt("hit or stay?")
+  answer = gets.chomp
+  break if answer == 'stay' || busted?(player_hand)   # the busted? method is not shown
+
+  prompt("You now have a total of #{player_hand += hit(deck)}")
+
+
+end
+
+if busted?(player_hand)
+prompt("busted")
+else
+  prompt("You chose to stay!")  # if player didn't bust, must have stayed to get here
+end
+
+# ... continue on to Dealer turn
+

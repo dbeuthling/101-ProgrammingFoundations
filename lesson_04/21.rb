fb4ca7c4e99b1cd8e5594e1437b0e686377fe2cd
Dan Beuthling <danbeuthling@gmail.com>
2016-03-13 21:01:01 -0500

    Refactored 21 a bit

--- lesson_04/21.rb
+++ lesson_04/21.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 card_faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
 card_suits = ['H', 'S', 'C', 'D']
 
@@ -7,29 +5,32 @@ def prompt(msg)
 	puts "=> #{msg}"
 end
 
-def players_cards(deck)
-	hand = deck.sample(2)
-	rank = hand.collect {|card| card[1] }
-	[rank[0] , rank[1]]
+def initialize_cards(suits, faces)
+  faces.product(suits).shuffle
+end
 
+new_deck = initialize_cards(card_suits, card_faces)
+
+def players_cards(deck)
+	hand = deck.pop(2)
 end
 
 
 def hit(cards)
-	cards.sample.values_at(1)
+	cards.pop
 end
 
 
 def dealer_cards(deck)
-	hand = deck.sample(2)
-	rank = hand.collect {|card| card[1] }
-	[rank[0] , rank[1]]
+	hand = deck.pop(2)
 end
 
 
 def find_value(cards)
 	sum = 0
-  cards.each do |value|
+  rank = cards.collect { |card| card[0] }
+
+  rank.each do |value|
     if value == "Ace"
       sum += 11
     elsif value.to_i == 0
@@ -37,68 +38,58 @@ def find_value(cards)
     else
       sum += value.to_i
     end
-  end
+    end
 
-	# sum = 0
-	# cards.each do |card|
-	# 	sum += VALUES.fetch(card)
-
-   	cards.count("Ace").times do
+   	rank.count("Ace").times do
     sum -= 10 if sum > 21
   end	
 	sum
 end
 
 def busted?(hand)
-	if hand > 21
-		return true
-	else
-		return false
-	end
+  hand > 21
 end
 
-loop do
-  deck = card_suits.product(card_faces)
-player_hand = players_cards(deck)
-dealer_hand = dealer_cards(deck)
-prompt("Dealer has a #{dealer_hand[0]} showing.")
-prompt("You have: #{player_hand[0]} and #{player_hand[1]}")
-prompt("For a total of #{find_value(player_hand)}")
+loop do # start blackjack
+  deck = new_deck
+  player_hand = players_cards(deck)
+  dealer_hand = dealer_cards(deck)
+  prompt("Dealer has a #{dealer_hand[0]} showing.")
+  prompt("You have: #{player_hand[0]} and #{player_hand[1]}")
+  prompt("For a total of #{find_value(player_hand)}")
 
+  hit_or_stay = nil
+  loop do # deal hands
+    loop do # player's turn
+    break if busted?(find_value(player_hand))
+      loop do
+        prompt("Do you (h)it or (s)tay?")
+        hit_or_stay = gets.chomp
+        break if ['h', 's'].include?(hit_or_stay)
+        prompt("You must enter an h or s!")
+      end
+    break if hit_or_stay.downcase.start_with?('s') || busted?(find_value(player_hand))
+    new_card = hit(deck)
+    player_hand << new_card
+    prompt("You drew a #{player_hand.last}")
+    prompt("You now have a total of #{find_value(player_hand)}")
+    end
 
+  if busted?(find_value(player_hand))
+    prompt("You busted!")
+    break
+  else
+    prompt("You chose to stay!")
+  end
 
-answer = nil
-loop do
-  loop do
-	break if busted?(find_value(player_hand))
-  prompt("hit or stay?")
-  answer = gets.chomp
-  break if answer.downcase.start_with?('s') || busted?(find_value(player_hand))
-  new_card = hit(deck)
-  player_hand << new_card
-  player_hand = player_hand.flatten!
-  prompt("You drew a #{player_hand.last}")
-  prompt("You now have a total of #{find_value(player_hand)}")
-
-
-end
-
-if busted?(find_value(player_hand))
-  prompt("You busted!")
-  break
-else
-  prompt("You chose to stay!")
-end
-
-
-prompt("Dealer has: #{dealer_hand[0]} and #{dealer_hand[1]}")
+  prompt("--------Dealer's Turn--------")
+  prompt("Dealer has: #{dealer_hand[1]} and #{dealer_hand[0]}")
   if find_value(dealer_hand) < 17
     prompt("Dealer has #{find_value(dealer_hand)}")
 		loop do 
-		  prompt("Dealer must hit.")
+		  prompt("-------Dealer must hit-------")
 		  new_card = hit(deck)
 		  dealer_hand << new_card
-		  dealer_hand = dealer_hand.flatten!
 		  prompt("Dealer drew a #{dealer_hand.last}")
 		  prompt("Dealer totals #{find_value(dealer_hand)}")
 		  break if find_value(dealer_hand) > 16 || busted?(find_value(dealer_hand))

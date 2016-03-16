a014be3df28aca80c082fc7e286c4ab49eb59eb9
Dan Beuthling <danbeuthling@gmail.com>
2016-03-15 23:07:10 -0500

    Refactored 21 according to example

--- lesson_04/21.rb
+++ lesson_04/21.rb
@@ -1,25 +1,13 @@
 require 'rubocop'
-card_faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
-card_suits = ['H', 'S', 'C', 'D']
+FACES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+VALUES = ['H', 'S', 'C', 'D']
 
 def prompt(msg)
   puts "=> #{msg}"
 end
 
-def initialize_cards(suits, faces)
-  faces.product(suits).shuffle
-end
-
-def players_cards(deck)
-  deck.pop(2)
-end
-
-def hit(cards)
-  cards.pop
-end
-
-def dealer_cards(deck)
-  deck.pop(2)
+def initialize_cards
+  FACES.product(VALUES).shuffle
 end
 
 def find_value(cards)
@@ -41,72 +29,98 @@ def find_value(cards)
 end
 
 def busted?(hand)
-  hand > 21
+  find_value(hand) > 21
 end
 
-loop do # play again?
-  deck = initialize_cards(card_suits, card_faces)
-  player_hand = players_cards(deck)
-  dealer_hand = dealer_cards(deck)
+def detect_result(dealer_hand, player_hand)
+  player_total = find_value(player_hand)
+  dealer_total = find_value(dealer_hand)
+  if player_total > 21
+    :player_busted
+  elsif dealer_total > 21
+    :dealer_busted
+  elsif dealer_total < player_total
+    :player
+  elsif dealer_total > player_total
+    :dealer
+  else
+    :push
+  end
+end
+
+def display_result(dealer_hand, player_hand)
+  result = detect_result(dealer_hand, player_hand)
+
+  case result
+  when :player_busted
+    prompt("You busted!")
+  when :dealer_busted
+    prompt("Dealer busted, you win!")
+  when :player
+    prompt("You win!")
+  when :dealer
+    prompt("Dealer wins!")
+  when :push
+    prompt("Push!")
+  end
+end
+
+def play_again?
+  prompt("Do you want to play again? (y or n)")
+  again = gets.chomp
+  again.downcase.start_with?('y')
+end
+
+
+loop do
+  prompt("Welcome to 21! Dealing cards...")
+  deck = initialize_cards
+  player_hand = deck.pop(2)
+  dealer_hand = deck.pop(2)
   prompt("Dealer has a #{dealer_hand[0]} showing.")
   prompt("You have: #{player_hand[0]} and #{player_hand[1]}")
   prompt("For a total of #{find_value(player_hand)}")
 
-  hit_or_stay = nil
-  loop do # game loop
-    loop do # player turns
-      break if busted?(find_value(player_hand))
-      loop do # hit or stay
-        prompt("Do you (h)it or (s)tay?")
-        hit_or_stay = gets.chomp
-        break if ['h', 's'].include?(hit_or_stay)
-        prompt("You must enter an h or s!")
-      end
-      break if hit_or_stay.downcase.start_with?('s') || busted?(find_value(player_hand))
-      new_card = hit(deck)
-      player_hand << new_card
-      prompt("You drew a #{player_hand.last}")
-      prompt("You now have a total of #{find_value(player_hand)}")
+  loop do # player
+    hit_or_stay = nil
+    loop do # hit or stay
+      prompt("Do you (h)it or (s)tay?")
+      hit_or_stay = gets.chomp
+      break if ['h', 's'].include?(hit_or_stay)
+      prompt("You must enter an h or s!")
     end
-
-    if busted?(find_value(player_hand))
-      prompt("You busted!")
-      break
-    else
-      prompt("You chose to stay!")
-    end
-
-    prompt("--------Dealer's Turn--------")
-    prompt("Dealer has: #{dealer_hand[1]} and #{dealer_hand[0]}")
-    if find_value(dealer_hand) < 17
-      prompt("Dealer has #{find_value(dealer_hand)}")
-      loop do
-        prompt("-------Dealer must hit-------")
-        new_card = hit(deck)
-        dealer_hand << new_card
-        prompt("Dealer drew a #{dealer_hand.last}")
-        prompt("Dealer totals #{find_value(dealer_hand)}")
-        break if find_value(dealer_hand) > 16 || busted?(find_value(dealer_hand))
-      end
-    end
-    prompt("You stayed with #{find_value(player_hand)}")
-    if busted?(find_value(dealer_hand))
-      prompt("Dealer busted, You win!")
-    else
-      prompt("Dealer stays with #{find_value(dealer_hand)}")
-      if find_value(dealer_hand) > find_value(player_hand)
-        prompt("Dealer wins!")
-      elsif find_value(dealer_hand) == find_value(player_hand)
-        prompt("Push")
-      else
-        prompt("You win!")
-      end
-    end
-    break
+    break if hit_or_stay.downcase.start_with?('s')
+    player_hand << deck.pop
+    prompt("You drew a #{player_hand.last}")
+    prompt("You now have a total of #{find_value(player_hand)}")
+    break if busted?(player_hand)
   end
 
-  prompt("Would you like to play again? (y or n)")
-  again = gets.chomp
-  break unless again.downcase.start_with?('y')
+  if busted?(player_hand)
+    display_result(dealer_hand, player_hand)
+    play_again? ? next : break
+  else
+    prompt("You chose to stay!")
+  end
+
+prompt("--------Dealer's Turn--------")
+prompt("Dealer has: #{dealer_hand[1]} and #{dealer_hand[0]}")
+  loop do
+    break if busted?(dealer_hand) || find_value(dealer_hand) > 16
+    prompt("Dealer has #{find_value(dealer_hand)}")
+    prompt("-------Dealer must hit-------")
+    dealer_hand << deck.pop
+    prompt("Dealer drew a #{dealer_hand.last}")
+  end
+
+    if busted?(dealer_hand)
+      prompt("Dealer has #{find_value(dealer_hand)}")
+      display_result(dealer_hand, player_hand)
+      play_again? ? next : break
+    end
+  prompt("You stayed with #{find_value(player_hand)}")
+  prompt("Dealer stays with #{find_value(dealer_hand)}")
+  display_result(dealer_hand, player_hand)
+  break unless play_again?
 end
 prompt("Thanks for playing!")
